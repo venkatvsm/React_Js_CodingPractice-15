bc09385359918c91fcad7e7ad2065f1108ac4d20
venkatvsm <venkatmani368@gmaoil.com>
2024-02-11 17:38:23 +0000

    Reviews App

---
+++ src/components/ReviewsCarousel/index.js
@@ -0,0 +1,68 @@
+// Write your code here
+import {Component} from 'react'
+import './index.css'
+
+class ReviewsCarousel extends Component {
+  state = {count: 0}
+
+  LeftIconTriggered = () => {
+    const {count} = this.state
+    if (count > 0) {
+      this.setState(prevState => ({count: prevState.count - 1}))
+    }
+  }
+
+  RightIconTriggered = () => {
+    const {count} = this.state
+    const {reviewsList} = this.props
+    if (count < reviewsList.length - 1) {
+      this.setState(prevState => ({count: prevState.count + 1}))
+    }
+  }
+
+  render() {
+    const {count} = this.state
+    const {reviewsList} = this.props
+    const {imgUrl, username, companyName, description} = reviewsList[count]
+    return (
+      <div className="bg_container">
+        <div className="card_container">
+          <h1 className="heading_review">Reviews</h1>
+          <div className="itemsContainer">
+            <button
+              type="button"
+              onClick={this.LeftIconTriggered}
+              className="btnEl"
+              testid="left Arrow"
+            >
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/left-arrow-img.png"
+                className="RighttIcon"
+                alt="left arrow"
+              />
+            </button>
+            <div className="bodyContainer">
+              <img src={`${imgUrl}`} className="ProfilePhoto" alt={username} />
+              <p>{username}</p>
+              <p>{companyName}</p>
+              <p>{description}</p>
+            </div>
+            <button
+              type="button"
+              onClick={this.RightIconTriggered}
+              className="btnEl"
+            >
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/right-arrow-img.png"
+                className="RighttIcon"
+                alt="right arrow"
+                testid="right Arrow"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+export default ReviewsCarousel
